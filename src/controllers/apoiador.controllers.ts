c50283ab46ba4c309806c258004689ca39bc47d9
isabelle9912 <isabellesaahirah28@gmail.com>
2024-11-29 09:57:35 -0300

    feat: controller de apoiador

--- src/controllers/apoiador.controllers.ts
+++ src/controllers/apoiador.controllers.ts
@@ -1,116 +1,57 @@
-// import { z } from "zod";
-// import { Request, Response } from "express";
-// import createUsersService from "../services/users/createUsers.service";
-// import deleteUsersService from "../services/users/deleteUsers.service";
-// import getAllUsersService from "../services/users/getAllUsers.service";
-// import updateUsersService from "../services/users/updateUsers.service";
-// import {
-//   iUserCommonCreate,
-//   iUserCreate,
-//   iUsersWithoutPass,
-//   iUserUpdate,
-// } from "../interfaces/user.interface";
+import { Request, Response } from "express";
 
-// import getUsersIdService from "../services/users/getUsersId.service";
+import getApoiadorIdService from "../services/apoiador/getApoiadorId.service";
+import getApoiadoresService from "../services/apoiador/getApoiadores.service";
+import { iApoiadorUpdate } from "../interfaces/apoiador.interface";
+import updateApoiadorService from "../services/apoiador/updateApoiador.service";
+import deleteApoiadorService from "../services/apoiador/deleteApoiador.service";
 
-// import forgotPasswordService from "../services/users/forgotPassword.service";
-// import resetPasswordService from "../services/users/resetPassword.service";
-// import getUsersUsernameService from "../services/users/getUsersUsername.service";
+const getApoiadorIdController = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  const id = Number(req.params.id);
+  const retrivedApoiador = await getApoiadorIdService(id);
 
-// const getUsersController = async (
-//   req: Request,
-//   res: Response
-// ): Promise<any> => {
-//   const username = req.params.username;
+  return res.status(200).json(retrivedApoiador);
+};
 
-//   const retrivedUser = await getUsersUsernameService(username);
+const getAllApoiadorController = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  const { page = 1, limit = 10 } = req.query;
+  const allUsers = await getApoiadoresService(Number(page), Number(limit));
 
-//   return res.status(200).json(retrivedUser);
-// };
+  return res.status(200).json(allUsers);
+};
 
-// const getUserIdController = async (
-//   req: Request,
-//   res: Response
-// ): Promise<any> => {
-//   const id = Number(req.params.id);
-//   const retrivedUser = await getUsersIdService(id);
+const updateApoiadorController = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  const payload: iApoiadorUpdate = req.body,
+    id = Number(req.params.id);
 
-//   return res.status(200).json(retrivedUser);
-// };
+  const updatedUser = await updateApoiadorService(id, payload);
 
-// const getAllUsersController = async (
-//   req: Request,
-//   res: Response
-// ): Promise<any> => {
-//   const allUsers = await getAllUsersService();
+  return res.status(200).json(updatedUser);
+};
 
-//   return res.status(200).json(allUsers);
-// };
+const deleteApoiadorController = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  const id = Number(req.params.id);
 
-// const createUsersController = async (
-//   req: Request,
-//   res: Response
-// ): Promise<any> => {
-//   const payload = req.body;
-  
-//   const createdUser = await createUsersService(payload);
+  await deleteApoiadorService(id);
 
-//   return res.status(201).json(createdUser);
-// };
+  return res.status(204).send();
+};
 
-// const updateUsersController = async (
-//   req: Request,
-//   res: Response
-// ): Promise<any> => {
-//   const payload: iUserUpdate = req.body,
-//     id = Number(req.params.id);
-
-//   const updatedUser = await updateUsersService(id, payload);
-
-//   return res.status(200).json(updatedUser);
-// };
-
-// const deleteUsersController = async (
-//   req: Request,
-//   res: Response
-// ): Promise<any> => {
-//   const id = Number(req.params.id);
-
-//   await deleteUsersService(id);
-
-//   return res.status(204).send();
-// };
-
-// const forgotPasswordController = async (
-//   req: Request,
-//   res: Response
-// ): Promise<any> => {
-//   const email: string = req.body.email;
-
-//   await forgotPasswordService(email);
-
-//   return res.status(200).send("Sucesso! O link para redefinir a sua senha foi enviado para o seu email.");
-// };
-
-// const resetPasswordController = async (
-//   req: Request,
-//   res: Response
-// ): Promise<any> => {
-//   const { token } = req.params;
-//   const { password } = req.body;
-
-//   await resetPasswordService(token, password);
-
-//   return res.status(200).send("Senha redefinida, bitch");
-// };
-
-// export {
-//   getUsersController,
-//   getUserIdController,
-//   getAllUsersController,
-//   createUsersController,
-//   updateUsersController,
-//   deleteUsersController,
-//   resetPasswordController,
-//   forgotPasswordController,
-// };
+export {
+  getApoiadorIdController,
+  getAllApoiadorController,
+  updateApoiadorController,
+  deleteApoiadorController,
+};
--- src/services/apoiador/getApoiadores.service.ts
+++ src/services/apoiador/getApoiadores.service.ts
@@ -3,8 +3,16 @@ import { iApoiador } from "../../interfaces/apoiador.interface";
 import Apoiador from "../../models/Apoiador";
 import { apoiadorSchema } from "../../schemas/apoiador.schema";
 
-const getApoiadoresService = async (): Promise<iApoiador[]> => {
-  const retrivedApoiadores = await Apoiador.findAll();
+const getApoiadoresService = async (
+  page: number,
+  limit: number
+): Promise<iApoiador[]> => {
+  const offset = (page - 1) * limit;
+
+  const retrivedApoiadores = await Apoiador.findAll({
+    offset,
+    limit,
+  });
 
   if (!retrivedApoiadores) {
     throw new AppError("Não existe nenhum apoiador registrado!", 404);
