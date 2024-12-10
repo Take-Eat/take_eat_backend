533aff223599f252e2a7af7a31be08e14bbfebdf
isabelle9912 <isabellesaahirah28@gmail.com>
2024-12-10 11:15:40 -0300

    feat: service de entregador

---
+++ src/services/entregador/getEntregadorId.service.ts
@@ -0,0 +1,43 @@
+import { AppError } from "../../errors";
+import { iEntregador } from "../../interfaces/entregador.interface";
+import Entregador from "../../models/Entregador";
+import { entregadorSchema } from "../../schemas/entregador.schema";
+
+/**
+  * Serviço para buscar um Entregador pelo seu ID.
+ \*
+  * @async
+  * @function getEntregadorIdService
+  * @param {number} id - Identificação do Entregador para retornar seus dados.
+  * @throws {AppError} Caso não seja encontrado o Entregador.
+  * @returns {Promise<iEntregador>} O Entregador encontrado e validado.
+ \*
+  * @example
+  * // Exemplo de chamada
+  * const retrivedEntregador = await getEntregadorIdService(2);
+  * // Exemplo de retorno
+ * {
+    id: number;
+    endereco: string;
+    idUsuario: number;
+    createdAt: Date;
+    updatedAt: Date;
+    nome: string;
+    cpf: string;
+    cnh: string;
+ * }
+ */
+
+const getEntregadorIdService = async (id: number): Promise<iEntregador> => {
+  const retrivedEntregador = await Entregador.findOne({
+    where: { id },
+  });
+
+  if (!retrivedEntregador) {
+    throw new AppError("Entregador nao encontrado!", 404);
+  }
+
+  return entregadorSchema.parse(retrivedEntregador);
+};
+
+export default getEntregadorIdService;
---
+++ src/services/entregador/getEntregadores.service.ts
@@ -0,0 +1,51 @@
+import { AppError } from "../../errors";
+import { iEntregador } from "../../interfaces/entregador.interface";
+import Doador from "../../models/Doador";
+import { entregadorSchema } from "../../schemas/entregador.schema";
+
+/**
+  * Serviço para buscar todos os Entregadores.
+ \*
+  * @async
+  * @function getEntregadoresService
+  * @param {number} page - Paginação das buscas.
+  * @param {number} limit - Quantidade de Entregadores que será retornado.
+  * @throws {AppError} Caso não seja encontrado nenhum entregador.
+  * @returns {Promise<iEntregador[]>}  Os Entregadores encontrados e validados.
+ \*
+  * @example
+  * // Exemplo de chamada
+  * const resultado = await getEntregadoresService(1, 15);
+ \*
+  * // Exemplo de retorno
+  * {
+    id: number;
+    endereco: string;
+    idUsuario: number;
+    createdAt: Date;
+    updatedAt: Date;
+    nome: string;
+    cpf: string;
+    cnh: string;
+  * }
+ */
+
+const getEntregadoresService = async (
+  page: number,
+  limit: number
+): Promise<iEntregador[]> => {
+  const offset = (page - 1) * limit;
+
+  const retrivedEntregadores = await Doador.findAll({
+    offset,
+    limit,
+  });
+
+  if (!retrivedEntregadores) {
+    throw new AppError("Não existe nenhum entregador registrado!", 404);
+  }
+
+  return entregadorSchema.array().parse(retrivedEntregadores);
+};
+
+export default getEntregadoresService;
