49bfbc83499c7ac07e026522d46dba1af554346d
isabelle9912 <isabellesaahirah28@gmail.com>
2024-12-18 15:07:34 -0300

    feat: service de get carteira by id

---
+++ src/services/carteira/getCarteira.service.ts
@@ -0,0 +1,40 @@
+import { AppError } from "../../errors";
+import { iCarteira } from "../../interfaces/carteira.interface";
+import Carteira from "../../models/Carteira";
+import { carteiraSchema } from "../../schemas/carteira.schema";
+
+/**
+  * Serviço para buscar uma Carteira pelo seu ID.
+ \*
+  * @async
+  * @function getCarteiraIdService
+  * @param {number} id - Identificação da Carteira para retornar seus dados.
+  * @throws {AppError} Caso não seja encontrada a Carteira.
+  * @returns {Promise<iCarteira>} A Carteira encontrada e validada.
+ \*
+  * @example
+  * // Exemplo de chamada
+  * const retrivedCarteira = await getCarteiraIdService(2);
+  * // Exemplo de retorno
+ * {
+ *   id: 2,
+ *   totalDoado: 200.50,
+ *   idApoiador: 1,
+ *   createdAt: "2024-12-01T12:00:00.000Z",
+ *   updatedAt: "2024-12-01T12:00:00.000Z",
+ * }
+ */
+
+const getCarteiraIdService = async (id: number): Promise<iCarteira> => {
+  const retrivedCarteira = await Carteira.findOne({
+    where: { id },
+  });
+
+  if (!retrivedCarteira) {
+    throw new AppError("Não foi possível encontrar a Carteira!", 404);
+  }
+
+  return carteiraSchema.parse(retrivedCarteira);
+};
+
+export default getCarteiraIdService;
